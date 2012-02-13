8c4602d07ce7f0709245b37e17c89649d4fea659
Thom Dedecko <tdedecko@gmail.com>
2012-02-12 17:09:23 -0800

    Articles are now being created successfully with validation on the title slug

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,21 +1,31 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError
+from django.db import IntegrityError
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=220, unique=True)
     author = models.ForeignKey(User)
-    publish_date = models.DateTimeField('date published')
-    modified_date = models.DateTimeField('date modified')
+    publish_date = models.DateTimeField('date published', auto_now_add=True)
+    modified_date = models.DateTimeField('date modified', auto_now=True)
     body = models.TextField()
     
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(Article, self).save(*args, **kwargs)
+            
 
 class ArticleForm(ModelForm):
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        if cleaned_data.has_key('title'):
+            slug = slugify(cleaned_data['title'])
+            if Article.objects.filter(slug=slug):
+                raise ValidationError("Article title is similar to existing articles. Try something new!")
+        return cleaned_data
+    
     class Meta:
         model = Article
         fields = ('title', 'body')
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from blog.wrappers import render_response
 from django.contrib.auth.decorators import login_required
-from blog.posts.models import ArticleForm
+from blog.posts.models import Article, ArticleForm
 
 def index(request):
     return render_response(request, 'index.html')
@@ -10,8 +10,10 @@ def index(request):
 @login_required
 def create_article(request):
     if request.method == 'POST':
-        form = ArticleForm(request.POST)
+        form = ArticleForm(request.POST, instance=Article(author=request.user))
         if form.is_valid():
+            
+            form.save()
             return HttpResponseRedirect(reverse('index'))
     else:
         form = ArticleForm()
